79e595e766160026d20a84cc23dff7297db0ba8c
Cyrus <cdastangoo@umass.edu>
2018-07-16 18:57:50 -0400

    Update pendulum.js

--- Modules/pendulum.js
+++ Modules/pendulum.js
@@ -51,8 +51,49 @@ function pendulum() {
 		ypos = canvas.height/5 + length.innerHTML*Math.cos(angle);
 	}
 
-    // redraw canvas
-    function redraw() {
+	function drawArrow(fromx, fromy, tox, toy) {
+		var rotate = Math.atan2(toy-fromy,tox-fromx);
+		ctx.strokeStyle = "cyan";
+		ctx.lineWidth = 1;
+		ctx.shadowBlur = 0;
+		ctx.beginPath();
+		ctx.moveTo(fromx, fromy);
+		ctx.lineTo(tox, toy);
+		ctx.lineTo(tox - 8*Math.cos(rotate-Math.PI/6), toy - 10*Math.sin(rotate-Math.PI/6));
+		ctx.moveTo(tox, toy);
+		ctx.lineTo(tox - 8*Math.cos(rotate+Math.PI/6), toy - 10*Math.sin(rotate+Math.PI/6));
+		ctx.stroke();
+	}
+
+	function Slider(x, y, min, max, step, value) {
+		this.x = x;
+		this.y = y;
+		this.min = min;
+		this.max = max;
+		this.step = step;
+		this.value = value;
+		this.dragged = false;
+		function draw() {
+			ctx.strokeStyle = "cyan";
+			ctx.fillStyle = "black";
+			ctx.lineWidth = 1;
+			if (dragged)
+				ctx.shadowBlur = 12;
+			else
+				ctx.shadowBlur = 0;
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x+100, y);
+			ctx.stroke();
+			ctx.beginPath();
+			let location = value/max*100;
+			ctx.arc(x+location, y, 5, 0, 2*Math.PI);
+			ctx.fill();
+			ctx.stroke();
+		}
+	}
+
+	function redraw() {
 		// draw canvas
 		ctx.fillStyle = "black";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -62,17 +103,17 @@ function pendulum() {
 		if (!clicked) {
 			acceleration = -1*g/length.innerHTML * Math.sin(angle);
 			velocity += acceleration;
-			velocity *= (1 - damping.innerHTML*0.01);
+			velocity *= (1 - damping.innerHTML*0.0015);
 			angle += velocity;
 		}
 		// handle mouse drag
 		else if (!paused) {
 			dx = mousex - canvas.width/2;
-			dy = mousey - canvas.height/5;
+			dy = mousey - canvas.height/4;
 			angle = Math.atan2(-1*dy, dx) + Math.PI/2;
 		}
 		xpos = canvas.width/2 + length.innerHTML*Math.sin(angle);
-		ypos = canvas.height/5 + length.innerHTML*Math.cos(angle);
+		ypos = canvas.height/4 + length.innerHTML*Math.cos(angle);
 
 		// draw string
 		ctx.shadowBlur = 0;
@@ -81,15 +122,17 @@ function pendulum() {
 		ctx.lineWidth = 1;
 		ctx.beginPath();
 		ctx.moveTo(xpos, ypos);
-		ctx.lineTo(canvas.width/2, canvas.height/5);
+		ctx.lineTo(canvas.width/2, canvas.height/4);
 		ctx.stroke();
 		ctx.beginPath();
-		ctx.arc(canvas.width/2, canvas.height/5, 3, 0, 2*Math.PI);
+		ctx.arc(canvas.width/2, canvas.height/4, 3, 0, 2*Math.PI);
 		ctx.fill();
-
+		
 		// draw ball
-		if (clicked)
+		if (clicked) {
+			drawArrow(xpos, ypos, xpos, ypos+mass.innerHTML*g*100);
 			ctx.shadowBlur = 12;
+		}
 		else
 			ctx.shadowBlur = 0;
 		ctx.shadowColor = "#66FF66";
@@ -108,15 +151,15 @@ function pendulum() {
 
 		// write labels
 		ctx.shadowBlur = 0;
-		ctx.font = "11pt Calibri";
-		ctx.fillStyle = "yellow";
+		ctx.font = "10pt Verdana";
+		ctx.fillStyle = "white";
 		ctx.textAlign = "center";
-        // angle
-        if (clicked || paused) {
-            var theta = angle*180/Math.PI % 360;
-		    ctx.fillText("Angle: \u03B8 = " + theta.toFixed(0) + "\xB0", canvas.width/2, canvas.height/5 - 15);
-        }
-        // force of tension
+		// angle
+		if (clicked || paused) {
+			var theta = angle*180/Math.PI % 360;
+			ctx.fillText("Angle: \u03B8 = " + theta.toFixed(0) + "\xB0", canvas.width/2, canvas.height/5 - 15);
+		}
+		// force of tension
 		var tension = mass.innerHTML/1000*9.81*Math.cos(angle);
 		ctx.fillText("Tension: F = " + tension.toFixed(2) + " N", canvas.width/2, canvas.height*4/5+25)
 		// period
@@ -125,7 +168,7 @@ function pendulum() {
 
 		setTimeout(redraw, 1);
 	}
-
+	
 	reset();
 	setTimeout(redraw, 1);
 
